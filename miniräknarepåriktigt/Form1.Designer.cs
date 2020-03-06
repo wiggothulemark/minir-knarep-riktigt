857c741b57e8bcd93e71429d30733a7b13657464
Wiggo Thulemark <thwi@ds.olofstrom.se>
2020-03-06 12:06:26 +0100

    Add project files.

---
+++ miniräknarepåriktigt/Form1.Designer.cs
@@ -0,0 +1,339 @@
+﻿namespace miniräknarepåriktigt
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.KnappNoll = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.KnappMinus = new System.Windows.Forms.Button();
+            this.KnappPlus = new System.Windows.Forms.Button();
+            this.button10 = new System.Windows.Forms.Button();
+            this.button11 = new System.Windows.Forms.Button();
+            this.button12 = new System.Windows.Forms.Button();
+            this.KnappLikaMed = new System.Windows.Forms.Button();
+            this.button14 = new System.Windows.Forms.Button();
+            this.button15 = new System.Windows.Forms.Button();
+            this.button16 = new System.Windows.Forms.Button();
+            this.button17 = new System.Windows.Forms.Button();
+            this.button18 = new System.Windows.Forms.Button();
+            this.button19 = new System.Windows.Forms.Button();
+            this.KnappTvå = new System.Windows.Forms.Button();
+            this.KnappEtt = new System.Windows.Forms.Button();
+            this.KnappKommatecken = new System.Windows.Forms.Button();
+            this.button23 = new System.Windows.Forms.Button();
+            this.Textbox = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // KnappNoll
+            // 
+            this.KnappNoll.Location = new System.Drawing.Point(12, 320);
+            this.KnappNoll.Name = "KnappNoll";
+            this.KnappNoll.Size = new System.Drawing.Size(114, 55);
+            this.KnappNoll.TabIndex = 0;
+            this.KnappNoll.Text = "0";
+            this.KnappNoll.UseVisualStyleBackColor = true;
+            this.KnappNoll.Click += new System.EventHandler(this.press);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(189, 195);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(54, 55);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(132, 195);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(54, 55);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(132, 134);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(54, 55);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "button4";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(189, 134);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(54, 55);
+            this.button5.TabIndex = 4;
+            this.button5.Text = "button5";
+            this.button5.UseVisualStyleBackColor = true;
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(249, 134);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(54, 55);
+            this.button6.TabIndex = 5;
+            this.button6.Text = "button6";
+            this.button6.UseVisualStyleBackColor = true;
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(249, 195);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(54, 55);
+            this.button7.TabIndex = 6;
+            this.button7.Text = "button7";
+            this.button7.UseVisualStyleBackColor = true;
+            // 
+            // KnappMinus
+            // 
+            this.KnappMinus.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.KnappMinus.Location = new System.Drawing.Point(192, 256);
+            this.KnappMinus.Name = "KnappMinus";
+            this.KnappMinus.Size = new System.Drawing.Size(54, 55);
+            this.KnappMinus.TabIndex = 7;
+            this.KnappMinus.Text = "-";
+            this.KnappMinus.UseVisualStyleBackColor = true;
+            // 
+            // KnappPlus
+            // 
+            this.KnappPlus.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.KnappPlus.Location = new System.Drawing.Point(192, 320);
+            this.KnappPlus.Name = "KnappPlus";
+            this.KnappPlus.Size = new System.Drawing.Size(54, 55);
+            this.KnappPlus.TabIndex = 8;
+            this.KnappPlus.Text = "+";
+            this.KnappPlus.UseVisualStyleBackColor = true;
+            // 
+            // button10
+            // 
+            this.button10.Location = new System.Drawing.Point(189, 73);
+            this.button10.Name = "button10";
+            this.button10.Size = new System.Drawing.Size(54, 55);
+            this.button10.TabIndex = 9;
+            this.button10.Text = "button10";
+            this.button10.UseVisualStyleBackColor = true;
+            // 
+            // button11
+            // 
+            this.button11.Location = new System.Drawing.Point(129, 73);
+            this.button11.Name = "button11";
+            this.button11.Size = new System.Drawing.Size(54, 55);
+            this.button11.TabIndex = 10;
+            this.button11.Text = "button11";
+            this.button11.UseVisualStyleBackColor = true;
+            // 
+            // button12
+            // 
+            this.button12.Location = new System.Drawing.Point(72, 73);
+            this.button12.Name = "button12";
+            this.button12.Size = new System.Drawing.Size(54, 55);
+            this.button12.TabIndex = 11;
+            this.button12.Text = "button12";
+            this.button12.UseVisualStyleBackColor = true;
+            // 
+            // KnappLikaMed
+            // 
+            this.KnappLikaMed.Location = new System.Drawing.Point(249, 256);
+            this.KnappLikaMed.Name = "KnappLikaMed";
+            this.KnappLikaMed.Size = new System.Drawing.Size(54, 119);
+            this.KnappLikaMed.TabIndex = 12;
+            this.KnappLikaMed.Text = "=";
+            this.KnappLikaMed.UseVisualStyleBackColor = true;
+            // 
+            // button14
+            // 
+            this.button14.Location = new System.Drawing.Point(12, 73);
+            this.button14.Name = "button14";
+            this.button14.Size = new System.Drawing.Size(54, 55);
+            this.button14.TabIndex = 13;
+            this.button14.Text = "button14";
+            this.button14.UseVisualStyleBackColor = true;
+            // 
+            // button15
+            // 
+            this.button15.Location = new System.Drawing.Point(132, 256);
+            this.button15.Name = "button15";
+            this.button15.Size = new System.Drawing.Size(54, 55);
+            this.button15.TabIndex = 14;
+            this.button15.Text = "button15";
+            this.button15.UseVisualStyleBackColor = true;
+            // 
+            // button16
+            // 
+            this.button16.Location = new System.Drawing.Point(72, 134);
+            this.button16.Name = "button16";
+            this.button16.Size = new System.Drawing.Size(54, 55);
+            this.button16.TabIndex = 15;
+            this.button16.Text = "button16";
+            this.button16.UseVisualStyleBackColor = true;
+            // 
+            // button17
+            // 
+            this.button17.Location = new System.Drawing.Point(12, 134);
+            this.button17.Name = "button17";
+            this.button17.Size = new System.Drawing.Size(54, 55);
+            this.button17.TabIndex = 16;
+            this.button17.Text = "button17";
+            this.button17.UseVisualStyleBackColor = true;
+            // 
+            // button18
+            // 
+            this.button18.Location = new System.Drawing.Point(72, 195);
+            this.button18.Name = "button18";
+            this.button18.Size = new System.Drawing.Size(54, 55);
+            this.button18.TabIndex = 17;
+            this.button18.Text = "button18";
+            this.button18.UseVisualStyleBackColor = true;
+            // 
+            // button19
+            // 
+            this.button19.Location = new System.Drawing.Point(12, 192);
+            this.button19.Name = "button19";
+            this.button19.Size = new System.Drawing.Size(54, 55);
+            this.button19.TabIndex = 18;
+            this.button19.Text = "button19";
+            this.button19.UseVisualStyleBackColor = true;
+            // 
+            // KnappTvå
+            // 
+            this.KnappTvå.Location = new System.Drawing.Point(72, 256);
+            this.KnappTvå.Name = "KnappTvå";
+            this.KnappTvå.Size = new System.Drawing.Size(54, 55);
+            this.KnappTvå.TabIndex = 19;
+            this.KnappTvå.Text = "2";
+            this.KnappTvå.UseVisualStyleBackColor = true;
+            // 
+            // KnappEtt
+            // 
+            this.KnappEtt.Location = new System.Drawing.Point(12, 256);
+            this.KnappEtt.Name = "KnappEtt";
+            this.KnappEtt.Size = new System.Drawing.Size(54, 55);
+            this.KnappEtt.TabIndex = 20;
+            this.KnappEtt.Text = "1";
+            this.KnappEtt.UseVisualStyleBackColor = true;
+            // 
+            // KnappKommatecken
+            // 
+            this.KnappKommatecken.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.KnappKommatecken.Location = new System.Drawing.Point(132, 320);
+            this.KnappKommatecken.Name = "KnappKommatecken";
+            this.KnappKommatecken.Size = new System.Drawing.Size(54, 55);
+            this.KnappKommatecken.TabIndex = 21;
+            this.KnappKommatecken.Text = ",";
+            this.KnappKommatecken.UseVisualStyleBackColor = true;
+            // 
+            // button23
+            // 
+            this.button23.Location = new System.Drawing.Point(249, 73);
+            this.button23.Name = "button23";
+            this.button23.Size = new System.Drawing.Size(54, 55);
+            this.button23.TabIndex = 22;
+            this.button23.Text = "button23";
+            this.button23.UseVisualStyleBackColor = true;
+            // 
+            // Textbox
+            // 
+            this.Textbox.Location = new System.Drawing.Point(12, 12);
+            this.Textbox.Name = "Textbox";
+            this.Textbox.Size = new System.Drawing.Size(291, 55);
+            this.Textbox.TabIndex = 23;
+            this.Textbox.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(318, 390);
+            this.Controls.Add(this.Textbox);
+            this.Controls.Add(this.button23);
+            this.Controls.Add(this.KnappKommatecken);
+            this.Controls.Add(this.KnappEtt);
+            this.Controls.Add(this.KnappTvå);
+            this.Controls.Add(this.button19);
+            this.Controls.Add(this.button18);
+            this.Controls.Add(this.button17);
+            this.Controls.Add(this.button16);
+            this.Controls.Add(this.button15);
+            this.Controls.Add(this.button14);
+            this.Controls.Add(this.KnappLikaMed);
+            this.Controls.Add(this.button12);
+            this.Controls.Add(this.button11);
+            this.Controls.Add(this.button10);
+            this.Controls.Add(this.KnappPlus);
+            this.Controls.Add(this.KnappMinus);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.KnappNoll);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button KnappNoll;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button KnappMinus;
+        private System.Windows.Forms.Button KnappPlus;
+        private System.Windows.Forms.Button button10;
+        private System.Windows.Forms.Button button11;
+        private System.Windows.Forms.Button button12;
+        private System.Windows.Forms.Button KnappLikaMed;
+        private System.Windows.Forms.Button button14;
+        private System.Windows.Forms.Button button15;
+        private System.Windows.Forms.Button button16;
+        private System.Windows.Forms.Button button17;
+        private System.Windows.Forms.Button button18;
+        private System.Windows.Forms.Button button19;
+        private System.Windows.Forms.Button KnappTvå;
+        private System.Windows.Forms.Button KnappEtt;
+        private System.Windows.Forms.Button KnappKommatecken;
+        private System.Windows.Forms.Button button23;
+        private System.Windows.Forms.RichTextBox Textbox;
+    }
+}
+
